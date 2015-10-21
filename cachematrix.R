752774d787681d1fb7ad9770d5c54c918eefffa9
Griecos <annalane.grieco@gmail.com>
2015-10-20 22:22:33 -0400

    Adding function contents for homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a "special" matrix which is 
+## really a list containing 4 functions
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<-y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inver) inv <<- inver
+  getInv <- function() inv
+  list(set = set, get = get, 
+       setInv = setInv, 
+       getInv = getInv)
+  }
 
-}
-
-
-## Write a short comment describing this function
+ ## Function to calculate the inverse of the "special" 
+ ##  matrix created above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <-x$getInv()
+  if(!is.null(inv)){
+    message("getting cached data, no need to recompute")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInv(inv)
+  inv
 }
+
+
